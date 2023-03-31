6b54563c2fa6d63d1a4b93c703f24dfc2baea55c
abgronie <abgronie8@gmail.com>
2023-03-31 08:48:32 +0300

    specified whose area and perimeter were being printed to the console

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ public class Main {
         Square square = new Square(10);
         double areaOfSquare = square.area();
         double perimeterOfSquare = square.perimeter();
-        System.out.printf("the area is: %.3f\n", areaOfSquare);
-        System.out.printf("the perimeter is: %.3f\n", perimeterOfSquare);
+        System.out.printf("the area of the square is: %.3f\n", areaOfSquare);
+        System.out.printf("the perimeter of the square is: %.3f\n", perimeterOfSquare);
 
         // rectangle
         // Rectangle rectanle = new Rectangle(10);
@@ -21,8 +21,8 @@ public class Main {
         Circle circle = new Circle(5);
         double areaOfCircle = circle.perimeter();
         double perimeterOfCircle = circle.perimeter();
-        System.out.printf("the area is: %.3f\n", areaOfCircle);
-        System.out.printf("the perimeter is: %.3f\n", perimeterOfCircle);
+        System.out.printf("the area of the circle is: %.3f\n", areaOfCircle);
+        System.out.printf("the perimeter of the circle is: %.3f\n", perimeterOfCircle);
 
     }
 }
